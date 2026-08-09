abc3c986496510673bd5377ad1e34f9e2ddbaa5c
Mihindu939 <bandaramihidu28@gmail.com>
2026-08-09 13:45:17 +0530

    Fix Vercel API routing

--- api/index.php
+++ api/index.php
@@ -1,9 +1,3 @@
 <?php
 
-header('Content-Type: application/json');
-
-echo json_encode([
-    'test' => true,
-    'uri' => $_SERVER['REQUEST_URI'] ?? null,
-    'path' => parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH),
-]);
+require __DIR__ . '/../public/index.php';
