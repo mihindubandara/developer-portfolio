440cc1a0f6fffd4bc5df0730f6d16dd00f2b0719
Mihindu939 <bandaramihidu28@gmail.com>
2026-08-09 12:12:27 +0530

    Add CV PDF

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -46,7 +46,7 @@
             <div class="mt-4">
                 <a id="github-link" href="https://github.com/mihindubandara" target="_blank" class="btn btn-outline-light me-2"><i class="fab fa-github"></i> GitHub</a>
                 <a id="linkedin-link" href="https://www.linkedin.com/in/mihindu-bandara-2256bb424" target="_blank" class="btn btn-outline-light me-2"><i class="fab fa-linkedin"></i> LinkedIn</a>
-                <a id="cv-link" href="/cv.pdf" target="_blank" class="btn btn-primary"><i class="fas fa-file-download"></i> Download CV</a>
+                <a id="cv-link" href="cv.pdf" target="_blank" class="btn btn-primary"><i class="fas fa-file-download"></i> Download CV</a>
             </div>
         </div>
     </section>
