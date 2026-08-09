175770e604eb5f66abc8a95add66fef7d110a332
Mihindu939 <bandaramihidu28@gmail.com>
2026-08-09 14:26:11 +0530

    Test Vercel API routing

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+header('Content-Type: application/json');
+
+echo json_encode([
+    'test' => true,
+    'uri' => $_SERVER['REQUEST_URI'] ?? null,
+    'method' => $_SERVER['REQUEST_METHOD'] ?? null,
+]);
+exit;
