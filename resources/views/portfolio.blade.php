b1a28f4b9d65a36fb4700603731c5584592397b3
Mihindu939 <bandaramihidu28@gmail.com>
2026-08-14 19:38:30 +0530

    Replace with latest ATS CV

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -58,7 +58,7 @@
             <div class="mt-4">
                 <a href="https://github.com/mihindubandara" target="_blank" class="btn btn-outline-light me-2 mb-2"><i class="fab fa-github"></i> GitHub</a>
                 <a href="https://www.linkedin.com/in/mihindu-bandara-2256bb424" target="_blank" class="btn btn-outline-light me-2 mb-2"><i class="fab fa-linkedin"></i> LinkedIn</a>
-                <a href="{{ asset('Mihindu_Bandara_CV.pdf') }}" download class="btn btn-primary mb-2">
+                <a href="{{ asset('Mihindu_Bandara_CV_.pdf') }}" download class="btn btn-primary mb-2">
                 <i class="fas fa-file-download"></i> Download CV
                 </a>
             </div>
